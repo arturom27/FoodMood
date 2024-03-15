2ce6531a490d48204e7a4415841e272b0c74638e
arturom27 <157868023+arturom27@users.noreply.github.com>
2024-03-14 23:11:02 -0700

    Add files via upload

---
+++ assets/profesionales/script.js
@@ -0,0 +1,120 @@
+"@context": "https://schema.org",
+"@type": "FAQPage",
+"mainEntity": []
+}</script>
+<script>(function($){
+$(document).ready(function(){
+accordion_Select accordion=$("#accordions-Select accordion   .items").accordion({
+event: "click",
+collapsible: true,
+heightStyle: "",
+animate: ("swing", 1000),
+navigation: true,
+active: 999,
+beforeActivate: function(event, ui){
+if(ui.newHeader[0]!=undefined){
+var disabled=ui.newHeader[0].getAttribute('disabled');
+console.log(disabled);
+if(disabled=='disabled'){
+event.preventDefault();
+}}
+},
+});
+})
+})(jQuery);</script>
+<script>!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+document,'script','https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '656672251147374');
+fbq('track', 'PageView');</script>
+<script>googletag.cmd.push(function(){
+googletag.defineSlot('/14808996/ES_DietaCoherente/DietaCoherente_MobileSite/banner', [[300, 600], [300, 250], [320, 50]], 'div-gpt-ad-1459788782720-0').addService(googletag.pubads());
+googletag.pubads().enableSingleRequest();
+googletag.pubads().collapseEmptyDivs();
+googletag.enableServices();
+});</script>
+<script>googletag.cmd.push(function(){
+googletag.defineSlot('/14808996/ES_DietaCoherente/DietaCoherente_MobileSite/richmedia', [1, 1], 'div-gpt-ad-1459788808479-0').addService(googletag.pubads());
+googletag.pubads().enableSingleRequest();
+googletag.pubads().collapseEmptyDivs();
+googletag.enableServices();
+});</script>
+<script src="//ads.themoneytizer.com/s/gen.js?type=6"></script>
+<script src="//ads.themoneytizer.com/s/requestform.js?siteId=103006&formatId=6"></script>
+<script src='https://www.dietacoherente.com/wp-content/plugins/related-posts-for-distinct-posts//js/rpdp-frontend-script.js?ver=6.1.4' id='rpdp-frontend-scripts-js'></script>
+<script src='//www.dietacoherente.com/wp-content/cache/wpfc-minified/jzxfh7he/8bsf8.js'></script>
+<script id='jquery-ui-datepicker-js-after'>jQuery(function(jQuery){jQuery.datepicker.setDefaults({"closeText":"Cerrar","currentText":"Hoy","monthNames":["enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre"],"monthNamesShort":["Ene","Feb","Mar","Abr","May","Jun","Jul","Ago","Sep","Oct","Nov","Dic"],"nextText":"Siguiente","prevText":"Anterior","dayNames":["domingo","lunes","martes","mi\u00e9rcoles","jueves","viernes","s\u00e1bado"],"dayNamesShort":["Dom","Lun","Mar","Mi\u00e9","Jue","Vie","S\u00e1b"],"dayNamesMin":["D","L","M","X","J","V","S"],"dateFormat":"d MM yy","firstDay":1,"isRTL":false});});</script>
+<script src='//www.dietacoherente.com/wp-content/cache/wpfc-minified/m1ce91qs/8bsx0.js'></script>
+<script id='wp-i18n-js-after'>wp.i18n.setLocaleData({ 'text direction\u0004ltr': [ 'ltr' ] });
+wp.i18n.setLocaleData({ 'text direction\u0004ltr': [ 'ltr' ] });</script>
+<script id='wc-blocks-google-analytics-js-translations'>(function(domain, translations){
+var localeData=translations.locale_data[ domain ]||translations.locale_data.messages;
+localeData[""].domain=domain;
+wp.i18n.setLocaleData(localeData, domain);
+})("woocommerce", {"translation-revision-date":"2022-11-01 19:37:36+0000","generator":"GlotPress\/4.0.0-alpha.3","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=2; plural=n!=1;","lang":"es"},"Viewing products":["Viendo productos"],"Product List":["Lista de productos"],"Change Cart Item Quantity":["Cambiar cantidad de art\u00edculos en el carrito"],"Remove Cart Item":["Eliminar art\u00edculo del carrito"],"Add to Cart":["A\u00f1adir al carrito"],"Payment Method":["M\u00e9todo de pago"],"Shipping Method":["M\u00e9todo de env\u00edo"]}},"comment":{"reference":"packages\/woocommerce-blocks\/build\/wc-blocks-google-analytics.js"}});</script>
+<script defer src='//www.dietacoherente.com/wp-content/cache/wpfc-minified/m0o2clk4/8eoxj.js'></script>
+<script>document.addEventListener('DOMContentLoaded',function(){
+jQuery(function($){
+$('.single_add_to_cart_button').on('click', function(){
+const item_data={"id":"#35967","name":"Dieta personalizada por patolog\u00edas","category":"Men\u00fas sanos y equilibrados","quantity":"1"};
+item_data.quantity=$("input.qty").val() ? $("input.qty").val():1;
+const selected_variation=google_analytics_integration_product_data[ $('input[name="variation_id"]').val() ];
+if(selected_variation!==undefined){
+item_data.id=selected_variation.id;
+item_data.variant=selected_variation.variant;
+}
+gtag('event', 'add_to_cart', {"items": [item_data]})
+});
+gtag('event', 'view_item_list', {"items":[{"id":"#26133","name":"Age F\u00f3rmula | Nutricosm\u00e9tico Antiedad","category":"Marca propia\/Nutricosmetica","list":"Lista de productos","list_position":"1"}]})
+$('.products .post-26133 a').on('click', function(){
+if(true===$(this).hasClass('add_to_cart_button')){
+gtag('event', 'add_to_cart', {"items":[{"id":"#26133","name":"Age F\u00f3rmula | Nutricosm\u00e9tico Antiedad","category":"Marca propia\/Nutricosmetica","list_position":"1","quantity":"1"}]})
+}else{
+gtag('event', 'select_content', {"items":[{"id":"#26133","name":"Age F\u00f3rmula | Nutricosm\u00e9tico Antiedad","category":"Marca propia\/Nutricosmetica","list_position":"1","quantity":"1"}]})
+}});
+gtag('event', 'view_item_list', {"items":[{"id":"#24458","name":"Pack Inicio Dieta Coherente | Epatofluid + B Ultra","category":"Suplementos\/Marca propia","list":"Lista de productos","list_position":"2"}]})
+$('.products .post-24458 a').on('click', function(){
+if(true===$(this).hasClass('add_to_cart_button')){
+gtag('event', 'add_to_cart', {"items":[{"id":"#24458","name":"Pack Inicio Dieta Coherente | Epatofluid + B Ultra","category":"Suplementos\/Marca propia","list_position":"2","quantity":"1"}]})
+}else{
+gtag('event', 'select_content', {"items":[{"id":"#24458","name":"Pack Inicio Dieta Coherente | Epatofluid + B Ultra","category":"Suplementos\/Marca propia","list_position":"2","quantity":"1"}]})
+}});
+gtag('event', 'view_item_list', {"items":[{"id":"#38861","name":"Pack 4 ebooks Dieta Coherente","category":"Ebooks","list":"Lista de productos","list_position":"3"}]})
+$('.products .post-38861 a').on('click', function(){
+if(true===$(this).hasClass('add_to_cart_button')){
+gtag('event', 'add_to_cart', {"items":[{"id":"#38861","name":"Pack 4 ebooks Dieta Coherente","category":"Ebooks","list_position":"3","quantity":"1"}]})
+}else{
+gtag('event', 'select_content', {"items":[{"id":"#38861","name":"Pack 4 ebooks Dieta Coherente","category":"Ebooks","list_position":"3","quantity":"1"}]})
+}});
+gtag('event', 'view_item_list', {"items":[{"id":"#35699","name":"Coletab 30 comp | Reduce el colesterol y los triglic\u00e9ridos","category":"Marca propia\/Salud y bienestar","list":"Lista de productos","list_position":"4"}]})
+$('.products .post-35699 a').on('click', function(){
+if(true===$(this).hasClass('add_to_cart_button')){
+gtag('event', 'add_to_cart', {"items":[{"id":"#35699","name":"Coletab 30 comp | Reduce el colesterol y los triglic\u00e9ridos","category":"Marca propia\/Salud y bienestar","list_position":"4","quantity":"1"}]})
+}else{
+gtag('event', 'select_content', {"items":[{"id":"#35699","name":"Coletab 30 comp | Reduce el colesterol y los triglic\u00e9ridos","category":"Marca propia\/Salud y bienestar","list_position":"4","quantity":"1"}]})
+}});
+gtag('event', 'view_item', {"items":[{"id":"#35967","name":"Dieta personalizada por patolog\u00edas","category":"Men\u00fas sanos y equilibrados","price":"40"}]})
+gtag('event', 'view_item_list', {"items":[{"id":"#35963","name":"Dieta personalizada para deportistas","category":"Men\u00fas sanos y equilibrados","list":"Lista de productos","list_position":"1"}]})
+$('.products .post-35963 a').on('click', function(){
+if(true===$(this).hasClass('add_to_cart_button')){
+gtag('event', 'add_to_cart', {"items":[{"id":"#35963","name":"Dieta personalizada para deportistas","category":"Men\u00fas sanos y equilibrados","list_position":"1","quantity":"1"}]})
+}else{
+gtag('event', 'select_content', {"items":[{"id":"#35963","name":"Dieta personalizada para deportistas","category":"Men\u00fas sanos y equilibrados","list_position":"1","quantity":"1"}]})
+}});
+gtag('event', 'view_item_list', {"items":[{"id":"#35967","name":"Dieta personalizada por patolog\u00edas","category":"Men\u00fas sanos y equilibrados","list":"Lista de productos","list_position":"2"}]})
+$('.products .post-35967 a').on('click', function(){
+if(true===$(this).hasClass('add_to_cart_button')){
+gtag('event', 'add_to_cart', {"items":[{"id":"#35967","name":"Dieta personalizada por patolog\u00edas","category":"Men\u00fas sanos y equilibrados","list_position":"2","quantity":"1"}]})
+}else{
+gtag('event', 'select_content', {"items":[{"id":"#35967","name":"Dieta personalizada por patolog\u00edas","category":"Men\u00fas sanos y equilibrados","list_position":"2","quantity":"1"}]})
+}});
+gtag('event', 'view_item_list', {"items":[{"id":"#56744","name":"Men\u00fas personalizados para adelgazar","category":"Men\u00fas sanos y equilibrados","list":"Lista de productos","list_position":"3"}]})
+$('.products .post-56744 a').on('click', function(){
+if(true===$(this).hasClass('add_to_cart_button')){
+gtag('event', 'add_to_cart', {"items":[{"id":"#56744","name":"Men\u00fas personalizados para adelgazar","category":"Men\u00fas sanos y equilibrados","list_position":"3","quantity":"1"}]})
+}else{
+gtag('event', 'select_content', {"items":[{"id":"#56744","name":"Men\u00fas personalizados para adelgazar","category":"Men\u00fas sanos y equilibrados","list_position":"3","quantity":"1"}]})
+}});
+});});
